16ff780733bf083b4e1e583324f0d12787bdbf5f
Abid <abidroid1@gmail.com>
2024-03-11 19:14:29 +0500

    screens added

---
+++ lib/screens/add_product_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AddProductScreen extends StatefulWidget {
+  const AddProductScreen({super.key});
+
+  @override
+  State<AddProductScreen> createState() => _AddProductScreenState();
+}
+
+class _AddProductScreenState extends State<AddProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Product'),
+      ),
+      body: ListView(children: [
+
+        TextField()
+      ],),
+    );
+  }
+}
